fd133465a542ffe00e40eb9542029759772d7fe7
charlene04 <charlesugbana04@gmail.com>
2020-08-19 12:56:36 +0100

    edited login route

--- middleware/isLoggedIn.js
+++ middleware/isLoggedIn.js
@@ -1,11 +1,11 @@
-
-
 function isLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
+	}else{
+		res.send({"error":"Unauthorised!"});
 	}
 
-	res.send({"error":"Unauthorised!"});
+	
 }
 
 module.exports = isLoggedIn;
